060d0aeb99cf328403a34f3fd4b607b6f040f4d9
James Cox <cox@gcdd.com>
2010-04-19 15:36:33 +0100

    user model started

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :users
+
   map.resources :countries
 
   # The priority is based upon order of creation: first created -> highest priority.
