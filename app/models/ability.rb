8035042e4329d22b16acee3f20d074fbf9637c7e
James Cox <cox@gcdd.com>
2010-04-27 16:25:58 +0100

    Fixed permissions on json calls issue

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,6 +4,8 @@ class Ability
   def initialize(user)
     user ||= User.new
     can :read, :all
+    can :areas, :all
+    can :iccaCountryDetails, :all
     if user.role? :admin
       can :manage, :all
     end
