91b78ca5ce1da12eb945a32a42a0b7c9953285e7
James Cox <th3james@gmail.com>
2010-09-03 09:46:55 +0100

    Use Sendgrid for mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,8 +37,12 @@ config.action_mailer.delivery_method = :smtp
 
 config.action_mailer.default_content_type = "text/html"
 
-config.action_mailer.smtp_settings = {
-        :address  => 'email-filter.unep-wcmc.org',
-        :port     => 25,
-        :domain   => 'unep-wcmc.org'
+ActionMailer::Base.smtp_settings = {
+  :address        => "smtp.sendgrid.net",
+  :port           => "25",
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => ENV['SENDGRID_DOMAIN'],
 }
+
