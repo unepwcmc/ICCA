07537bf4ca9b86de3bbb9d16ac45f37bbff47bab
James Cox <th3james@gmail.com>
2010-09-03 10:05:42 +0100

    Changed from address

--- app/models/emailer.rb
+++ app/models/emailer.rb
@@ -1,17 +1,8 @@
 class Emailer < ActionMailer::Base
 
-  def account_confirmation(user)
-    @user = user
-    recipients  @user.email
-    from        "ICCA Registry (iccaregistry.org)"
-    subject     "Account confirmation from ICCARegistry.org"
-    body        :user => user
-  end
-
   def mail_user_new_password(contributor, new_password)
     recipients  contributor.email
-    from        "Global Coral Disease Database (coraldisease.org)"  
-    from        "ICCA Registry (iccaregistry.org)"
+    from        "iccaregistry@unep-wcmc.org"
     subject     "ICCA Password Reset"
     body        :contributor => contributor, :new_password => new_password
   end
