dd8c85055b4f9af75ba5907c6caf36a0d4fe9a50
James Cox <th3james@gmail.com>
2010-12-03 14:14:57 +0000

    Using bundler

---
+++ Gemfile
@@ -0,0 +1,11 @@
+source :rubygems
+  gem "rails", '2.3.10'
+  gem "sqlite3-ruby", "1.3.2"
+  gem "pg"
+  gem "formtastic"
+  gem "authlogic"
+  gem "paperclip"
+  gem "aws-s3"
+  gem "cancan"
+  gem "rake"
+  gem "ruby-debug"
--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.10' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -18,13 +18,7 @@ Rails::Initializer.run do |config|
   # config.gem "bj"
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
-  config.gem "formtastic"
-  config.gem "authlogic"
-  config.gem "paperclip"
-  config.gem "cancan"
-  config.gem "aws-s3", :lib => "aws/s3"
   
-
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
