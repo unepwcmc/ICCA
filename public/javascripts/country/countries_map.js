1209be5de7bf7bc1540ed02f0023c35725d65948
James Cox <th3james@gmail.com>
2011-10-03 16:22:55 +0100

    Javascript countries map

---
+++ public/javascripts/country/countries_map.js
@@ -0,0 +1,51 @@
+var map;
+var bounds = new google.maps.LatLngBounds();
+
+function zoomIn() {
+    map.setZoom(map.getZoom() +1);
+}
+
+function zoomOut() {
+    map.setZoom(map.getZoom() -1);
+}
+
+function changeMapType(type) {
+    if (type=="satellite") {
+        if (!$("#satellite").hasClass('current')) {
+            $("#terrain").removeClass('current');
+            $("#satellite").addClass('current');
+            map.setMapTypeId(google.maps.MapTypeId.SATELLITE);
+        }
+    } else {
+        if (!$("#terrain").hasClass('current')) {
+            $("#terrain").addClass('current');
+            $("#satellite").removeClass('current');
+            map.setMapTypeId(google.maps.MapTypeId.TERRAIN);
+        }
+    }
+}
+
+$(document).ready(function() {
+    var country_id = $('span#country_id').text();
+    var myLatlng = new google.maps.LatLng(0,0);
+    var myOptions = {
+        zoom: 4,
+        center: myLatlng,
+        disableDefaultUI: true,
+        mapTypeId: google.maps.MapTypeId.TERRAIN
+    }
+    map = new google.maps.Map(document.getElementById("countries_map"), myOptions);
+
+    $.getJSON('/iccaCountryDetails.json',function(data){
+
+        for (var i=0; i<data.iccas.length; i++) {
+            var latlng = new google.maps.LatLng(data.iccas[i].lat,data.iccas[i].lng);
+            var marker = new Marker(latlng,data.iccas[i],map);
+            bounds.extend(latlng);
+        }
+        map.fitBounds(bounds);
+        console.log(data);
+    });
+});
+
+
