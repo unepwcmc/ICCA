774aab8b714352d72867c6a5b564b39664c016ee
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2016-01-25 19:52:11 +0000

    add snippet in environment for ruby>=2.0

--- config/environment.rb
+++ config/environment.rb
@@ -1,4 +1,25 @@
 # Be sure to restart your server when you modify this file
+#
+if RUBY_VERSION >= '2.0.0'
+  module Gem
+    def self.source_index
+      sources
+    end
+
+    def self.cache
+      sources
+    end
+
+    SourceIndex = Specification
+
+    class SourceList
+      # If you want vendor gems, this is where to start writing code.
+      def search(*args); []; end
+      def each(&block); end
+      include Enumerable
+    end
+  end
+end
 
 # Specifies gem version of Rails to use when vendor/rails is not present
 RAILS_GEM_VERSION = '2.3.18' unless defined? RAILS_GEM_VERSION
@@ -18,7 +39,7 @@ Rails::Initializer.run do |config|
   # config.gem "bj"
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
-  
+
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
