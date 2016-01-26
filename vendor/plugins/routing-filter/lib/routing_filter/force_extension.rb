5618f1cb93fda301923574a3f5e057bd5faa2d47
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2016-01-26 12:14:17 +0000

    use Object#tap in routing-filter plugin

--- vendor/plugins/routing-filter/lib/routing_filter/force_extension.rb
+++ vendor/plugins/routing-filter/lib/routing_filter/force_extension.rb
@@ -16,7 +16,7 @@ module RoutingFilter
     end
 
     def around_generate(*args, &block)
-      returning yield do |result|
+      yield.tap do |result|
         url = result.is_a?(Array) ? result.first : result
         append_extension!(url) if append_extension?(url)
       end
@@ -28,11 +28,11 @@ module RoutingFilter
         path.sub! /\.#{extension}$/, ''
         $1
       end
-      
+
       def append_extension?(url)
         !(url.blank? || excluded?(url) || mime_extension?(url))
       end
-      
+
       def excluded?(url)
         case exclude
         when Regexp
@@ -41,11 +41,11 @@ module RoutingFilter
           exclude.call(url)
         end
       end
-      
+
       def mime_extension?(url)
         url =~ /\.#{Mime::EXTENSION_LOOKUP.keys.join('|')}(\?|$)/
       end
-      
+
       def append_extension!(url)
         url.replace url.sub(/(\?|$)/, ".#{extension}\\1")
       end
