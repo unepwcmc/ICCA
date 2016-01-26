e86d4ce31f301f800f5821983b4e109f0f780ac3
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2016-01-26 11:20:57 +0000

    fixed weird document_field migration

--- db/migrate/20100901105405_rename_document_field_file_to_doc_file.rb
+++ db/migrate/20100901105405_rename_document_field_file_to_doc_file.rb
@@ -1,9 +1,9 @@
 class RenameDocumentFieldFileToDocFile < ActiveRecord::Migration
   def self.up
     rename_column :documents, :file_file_name, :doc_file_name
-    rename_column :documents, :file_content_type, :doc_file_name
-    rename_column :documents, :file_file_size, :doc_file_name
-    rename_column :documents, :file_updated_at, :doc_file_name
+    rename_column :documents, :file_content_type, :doc_content_type
+    rename_column :documents, :file_file_size, :doc_file_size
+    rename_column :documents, :file_updated_at, :doc_updated_at
   end
 
   def self.down
